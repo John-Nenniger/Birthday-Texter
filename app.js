9ecfb1b0b0d55c0f18b2f11c28e367b3343276d8
John Nenniger <JPNENNIGER@GMAIL.com>
2018-02-06 14:39:29 -0800

    created db locally and added knex, next i need to create a table with knex

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-type', 'text/plain');
-  res.end(`Hello World/n`);
+  res.end(`This is just a server, please move on.`);
 })
 
 server.listen(port, hostname, () => {
